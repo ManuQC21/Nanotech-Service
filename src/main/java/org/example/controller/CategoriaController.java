b44aed26f698522b7429f718790eecd49aea19fe
ManuQC21 <mquispecueva1@upao.edu.pe>
2023-07-12 07:35:48 -0500

    Update CategoriaController.java

--- src/main/java/org/example/controller/CategoriaController.java
+++ src/main/java/org/example/controller/CategoriaController.java
@@ -5,7 +5,7 @@ import org.example.utils.GenericResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//.
 @RestController
 @RequestMapping("api/categoria")
 public class CategoriaController {
